2d0e83b21d4c52f4e6d55341d17b771acde263d2
RedJohn-2 <LakadMatataag@mail.ru>
2024-06-25 18:28:30 +0300

    New database Migration. Email has alternative key

--- DOTA2TierList.Persistence/Migrations/20240625152707_UserTableFixMigration.Designer.cs
+++ DOTA2TierList.Persistence/Migrations/20240625152707_UserTableFixMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DOTA2TierList.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DOTA2TierList.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20240625152707_UserTableFixMigration")]
+    partial class UserTableFixMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DOTA2TierList.Persistence/Migrations/20240625152707_UserTableFixMigration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DOTA2TierList.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserTableFixMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_Users_Email",
+                table: "Users",
+                column: "Email");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_Users_Email",
+                table: "Users");
+        }
+    }
+}
